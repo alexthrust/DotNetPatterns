fb619c4dee5add5135451ecffd5c36b9a5839be0
Alexey Blauta <alexthrust@gmail.com>
2018-10-06 15:46:51 +0300

    Init

---
+++ DotNetPatterns.Exec/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetPatterns.Exec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
